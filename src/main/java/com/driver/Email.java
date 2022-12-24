e0d705d35943f5d8d4bea384be89217cb71edfc0
Lizter <lizterdsouza@gmail.com>
2022-12-24 17:55:07 +0530

    code

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,6 +1,9 @@
 package com.driver;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Email {
+public class Email  {
 
     private String emailId;
     private String password;
@@ -18,12 +21,25 @@ public class Email {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword){
+    public void changePassword(String oldPassword, String newPassword) {
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        // 1. It contains at least 8 characters
-        // 2. It contains at least one uppercase letter
-        // 3. It contains at least one lowercase letter
-        // 4. It contains at least one digit
-        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if (oldPassword.equals(password)) {  //me
+
+
+            // 1. It contains at least 8 characters
+            // 2. It contains at least one uppercase letter
+            // 3. It contains at least one lowercase letter
+            // 4. It contains at least one digit
+            // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+            String regex = "^(?=.*[a-z])(?=." + "*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(newPassword);
+            if (m.matches())
+            {
+                password= newPassword;
+            }
+
+
+        }
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
@@ -12,11 +13,14 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId);
+        this.inboxCapacity= Integer.MAX_VALUE;
+        calendar=new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
+        calendar.add(meeting);
 
     }
 
@@ -26,5 +30,25 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
+        if(calendar.size()==0)return 0;
+        Collections.sort(calendar,new Comparator<Meeting>()
+        {
+            public int compare(Meeting a,Meeting b)// sorting according to the end time of meetings
+            {
+                return a.getEndTime().compareTo(b.getEndTime());
+            }
+        });
+
+        LocalTime limit=calendar.get(0).getEndTime();
+        int maxmeet=1;
+        for(int i =1 ; i < calendar.size() ; i++)
+        {
+            if(calendar.get(i).getStartTime() .compareTo(limit) >0)
+            {
+                maxmeet++;
+                limit=calendar.get(i).getEndTime();
+            }
+        }
+        return maxmeet;
     }
 }
