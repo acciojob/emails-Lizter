f00094b65810528253d5a3c5e3eb59d9001afb88
Lizter <lizterdsouza@gmail.com>
2023-01-01 13:04:19 +0530

    codef

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,9 +1,6 @@
 package com.driver;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class Email  {
+public class Email {
 
     private String emailId;
     private String password;
@@ -21,25 +18,54 @@ public class Email  {
         return password;
     }
 
-    public void changePassword(String oldPassword, String newPassword) {
+    public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
-        if (oldPassword.equals(password)) {  //me
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
 
-
-            // 1. It contains at least 8 characters
-            // 2. It contains at least one uppercase letter
-            // 3. It contains at least one lowercase letter
-            // 4. It contains at least one digit
-            // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-            String regex = "^(?=.*[a-z])(?=." + "*[A-Z])(?=.*\\d)" + "(?=.*[-+_!@#$%^&*., ?]).+$";
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(newPassword);
-            if (m.matches())
-            {
-                password= newPassword;
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                System.out.println("Password changed successfully!");
+                this.password = newPassword;
             }
-
-
+            else{
+                System.out.println("The new password is not valid!");
+            }
         }
+        else{
+            System.out.println("The given password does not match current password!");
+        }
+    }
+
+    private Boolean isValid(String password){
+        Boolean capitalLetter = false;
+        Boolean smallLetter = false;
+        Boolean digit = false;
+        Boolean specialCharacter = false;
+
+        if(password.length() < 8){
+            return false;
+        }
+
+        for(int i = 0; i<password.length(); i++){
+            char ch = password.charAt(i);
+            if((ch >= 'A') && (ch <= 'Z')){
+                capitalLetter = true;
+            }
+            else if((ch >= 'a') && (ch <= 'z')){
+                smallLetter = true;
+            }
+            else if((ch >= '0') && (ch <= '9')){
+                digit = true;
+            }
+            else specialCharacter = true;
+        }
+
+        if(capitalLetter && smallLetter && digit && specialCharacter)
+            return true;
+        return false;
     }
 }
--- src/main/java/com/driver/Meeting.java
+++ src/main/java/com/driver/Meeting.java
@@ -6,26 +6,16 @@ public class Meeting {
     private LocalTime startTime;
     private LocalTime endTime;
 
-    public LocalTime getStartTime() {
-        return startTime;
+    public Meeting(LocalTime startTime, LocalTime endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
+    public LocalTime getStartTime() {
+        return startTime;
     }
 
     public LocalTime getEndTime() {
         return endTime;
     }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public Meeting(LocalTime startTime, LocalTime endTime){
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-
 }
